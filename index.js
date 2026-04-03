e28c3851701898d80d27ba4ee43f0eb2365da582
esteraant <EsteraAntal@gmail.com>
2026-04-03 22:11:00 +0300

    Etapa 4

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const express= require("express");
 const path= require("path");
 const fs=require("fs");
 const sass=require("sass");
+const sharp=require("sharp");
+
 
 app= express();
-app.set("view engine", "ejs")
+app.set("view engine", "ejs") //motorul de randare
 
+///3. Calea folderului, calea fisierului si folderul curent
 console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
-app.get(["/", "/index", "/home"], function(req, res){ //cerinta 8
-    console.log("get on '/' ")
-    res.render("pagini/index")
-})
+// app.get(["/", "/index", "/home"], function(req, res){ //cerinta 8
+//     console.log("get on '/' ")
+//     res.render("pagini/index") //trimite pagina catre utilizator
+// })
 
+// Cerinta 13
 obGlobal={
     obErori:null,
     obImagini:null,
@@ -23,6 +27,8 @@ obGlobal={
     folderBackup: path.join(__dirname,"backup"),
 }
 
+
+//  Cerinta 20
 vect_foldere=["temp", "logs", "backup", "fisiere_uploadate"]
 
 for(let folder of vect_foldere){
@@ -32,12 +38,14 @@ for(let folder of vect_foldere){
     }
 }
 
-app.use("/resurse", express.static(path.join(__dirname, "resurse")));
+app.use("/resurse", express.static(path.join(__dirname, "resurse")));  //6. Folder static
+app.use("/dist", express.static(path.join(__dirname, "node_modules/dist")));
 //orice caut in /resurse cauta in acest folder
 
 app.get(["/", "/index","/home"], function(req, res){
     res.render("pagini/index", {
-        ip:req.ip,
+        ip:req.ip,                      // Cerinta 16
+        imagini:obGlobal.obImagini.imagini
     });
 });
 
@@ -45,6 +53,8 @@ app.get("/despre", function(req, res){
     res.render("pagini/despre");
 });
 
+
+// Cerinta 19
 app.get("/favicon.ico", function(req, res){
     res.sendFile(path.join(__dirname, "resurse/imagini/favicon/favicon.ico"))
 });
@@ -69,18 +79,17 @@ function afisareEroare(res, identificator, titlu, text, imagine){
     //TO DO cautam eroarea dupa identificator
     let eroare=obGlobal.obErori.info_erori.find((elem)=>  //in stilul: v.find(x)=> x%8==0
         elem.identificator==identificator
-    );
+    )
     //daca sunt setate titlu, text, imagine, le folosim, 
     //altfel folosim cele din fisierul json pentru eroarea gasita
     //daca nu o gasim, afisam eroarea default
     let errDefault=obGlobal.obErori.eroare_default;
-    if (eroare?.status){
+    if (eroare?.status)
         res.status(eroare.identificator);
-    }
-    res.render("pagini/eroare",{
+    res.render("pagini/eroare",{            // Cerinta 14
         imagine: imagine || eroare?.imagine || errDefault.imagine,
-        titlu: titlu || titlu?.titlu || errDefault.titlu,
-        text: text || text?.text || errDefault.text,
+        titlu: titlu || eroare?.titlu || errDefault.titlu,
+        text: text || eroare?.text || errDefault.text,
     });
 
 }
@@ -88,6 +97,32 @@ function afisareEroare(res, identificator, titlu, text, imagine){
 app.get("/eroare", function(req,res){
     afisareEroare(res, 404, "Eroare 404 - Pagina nu a fost gasita");
 });
+
+
+function initImagini(){
+    var continut= fs.readFileSync(path.join(__dirname,"resurse/json/galerie.json")).toString("utf-8");
+
+    obGlobal.obImagini=JSON.parse(continut);
+    let vImagini=obGlobal.obImagini.imagini;
+    let caleGalerie=obGlobal.obImagini.cale_galerie
+
+    let caleAbs=path.join(__dirname,caleGalerie);
+    let caleAbsMediu=path.join(caleAbs, "mediu");
+    if (!fs.existsSync(caleAbsMediu))
+        fs.mkdirSync(caleAbsMediu);
+    
+    for (let imag of vImagini){
+        [numeFis, ext]=imag.fisier.split("."); //"ceva.png" -> ["ceva", "png"]
+        let caleFisAbs=path.join(caleAbs,imag.fisier);
+        let caleFisMediuAbs=path.join(caleAbsMediu, numeFis+".webp");
+        sharp(caleFisAbs).resize(300).toFile(caleFisMediuAbs);
+        imag.fisier_mediu=path.join("/", caleGalerie, "mediu", numeFis+".webp" )
+        imag.fisier=path.join("/", caleGalerie, imag.fisier )
+        
+    }
+    // console.log(obGlobal.obImagini)
+}
+initImagini();
 
 app.get("/*pagina", function(req, res){
     console.log("Pagina ceruta", req.url);
@@ -99,7 +134,7 @@ app.get("/*pagina", function(req, res){
         afisareEroare(res, 400);
         return;
     }
-    try{
+    try{  // 10. Eroare 404
         res.render("pagini" + req.url, function(err, rezRandare){
             if(err){
                 if(err?.message.includes("Failed to lookup view")){
@@ -121,5 +156,5 @@ app.get("/*pagina", function(req, res){
     }
 });
 
-app.listen(4080);
+app.listen(8080);
 console.log("Serverul a pornit!");
